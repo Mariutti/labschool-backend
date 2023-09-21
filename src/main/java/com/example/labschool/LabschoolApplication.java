bebbd3caf87ac0bb48e21d42d651aff1d82eaea2
Pedro Alencar <pedromariutti@gmail.com>
2023-09-20 21:17:22 -0300

    add a logger

--- src/main/java/com/example/labschool/LabschoolApplication.java
+++ src/main/java/com/example/labschool/LabschoolApplication.java
@@ -1,13 +1,20 @@
 package com.example.labschool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LabschoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LabschoolApplication.class, args);
-	}
 
+	private static Logger LOGGER = LoggerFactory.getLogger(LabschoolApplication.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("INICIANDO API LABSCHOOL...");
+		SpringApplication.run(LabschoolApplication.class, args);
+		LOGGER.info("API INICIADA COM SUCESSO.");
+		LOGGER.debug("Log Debug");
+	}
 }
